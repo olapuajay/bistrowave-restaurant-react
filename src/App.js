064fb2e4f7318a8425e1c89f93a11ef669305c41
Olapu Ajay <olapuajay@gmail.com>
2024-11-15 13:20:12 +0530

    Wrapping the project

--- src/App.js
+++ src/App.js
@@ -9,57 +9,53 @@ import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 
 function App() {
-
-  
-
   return (
     <div>
-          <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
-            <Container>
-              <Navbar.Brand>
-                <Link to="/" className="navbar-brand">
-                  <h4 className="btn btn-success fw-bold">BISTROWAVE</h4>
-                </Link>
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto justify-content-end w-100">
-                  <Nav.Link as={Link} to="/" className="active text-uppercase">
-                    Home
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/menu" className="text-uppercase">
-                    Menu
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/about" className="text-uppercase">
-                    About
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/contact" className="text-uppercase">
-                    Contact
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+      <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/" className="navbar-brand">
+              <h4 className="btn btn-success fw-bold">BISTROWAVE</h4>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto justify-content-end w-100">
+              <Nav.Link as={Link} to="/" className="active text-uppercase">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/menu" className="text-uppercase">
+                Menu
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about" className="text-uppercase">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact" className="text-uppercase">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
-          <footer className="bg-body-tertiary">
-            <p className="p-3 m-0 text-center">
-              &copy; Bistrowave 2024. All rights reserved. Developed by{" "}
-              <a
-                href="https://github.com/olapuajay"
-                className="text-decoration-none"
-              >
-                AJAY
-              </a>{" "}
-            </p>
-          </footer>
-          
+      <footer className="bg-body-tertiary">
+        <p className="p-3 m-0 text-center">
+          &copy; Bistrowave 2024. All rights reserved. Developed by{" "}
+          <a
+            href="https://github.com/olapuajay"
+            className="text-decoration-none"
+          >
+            AJAY
+          </a>{" "}
+        </p>
+      </footer>
     </div>
   );
 }
