4951e00245948e601594fe8ef598f791cf8c9745
Olapu Ajay <olapuajay@gmail.com>
2024-11-15 12:17:59 +0530

    Add loading screen component and implement loading state in App

--- src/App.js
+++ src/App.js
@@ -7,55 +7,72 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
+import Loading from './components/Loading'
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  })
+
   return (
     <div>
-      <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" className="navbar-brand">
-              <h4 className="btn btn-success fw-bold">BISTROWAVE</h4>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto justify-content-end w-100">
-              <Nav.Link as={Link} to="/" className="active text-uppercase">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/menu" className="text-uppercase">
-                Menu
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about" className="text-uppercase">
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="text-uppercase">
-                Contact
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      { loading && <Loading /> }
+      { !loading && (
+        <>
+          <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
+            <Container>
+              <Navbar.Brand>
+                <Link to="/" className="navbar-brand">
+                  <h4 className="btn btn-success fw-bold">BISTROWAVE</h4>
+                </Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto justify-content-end w-100">
+                  <Nav.Link as={Link} to="/" className="active text-uppercase">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/menu" className="text-uppercase">
+                    Menu
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/about" className="text-uppercase">
+                    About
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/contact" className="text-uppercase">
+                    Contact
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
 
-      <footer className="bg-body-tertiary">
-        <p className="p-3 m-0 text-center">
-          &copy; Bistrowave 2024. All rights reserved. Developed by{" "}
-          <a
-            href="https://github.com/olapuajay"
-            className="text-decoration-none"
-          >
-            AJAY
-          </a>{" "}
-        </p>
-      </footer>
+          <footer className="bg-body-tertiary">
+            <p className="p-3 m-0 text-center">
+              &copy; Bistrowave 2024. All rights reserved. Developed by{" "}
+              <a
+                href="https://github.com/olapuajay"
+                className="text-decoration-none"
+              >
+                AJAY
+              </a>{" "}
+            </p>
+          </footer>
+        </>
+      ) }
     </div>
   );
 }
